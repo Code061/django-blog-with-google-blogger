4c754544e0871b47e3e020642824243ae5965896
Hari_Vardhan <120207793+Code061@users.noreply.github.com>
2025-09-25 10:18:48 +0530

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,7 +1,7 @@
 import requests  # type: ignore
 
 URL=f'https://www.googleapis.com/blogger/v3/blogs/2399953/posts'
-API_KEY = 'AIzaSyCV3RhAfKZv_w_1VUnpCOfXb26pgDGqA2w'
+API_KEY = '##########################################'
 parameters = {
     'max_results': 5,
     'fields': 'items',
